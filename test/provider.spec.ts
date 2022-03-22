99a10254ba474ea1079966875a0f18263fd7bb11
John Denver <chogathkk@gmail.com>
2022-03-22 15:50:11 +0800

    feat: add dust container & provider 😄

--- test/provider.spec.ts
+++ test/provider.spec.ts
@@ -3,7 +3,7 @@ import { isMainThread } from 'worker_threads';
 
 import { describe, it, expect } from '@jest/globals';
 
-import { DustThread } from '../src/common/base';
+import { DustThread, DustContainer } from '../src/common/base';
 
 const workers = {
   'task': resolve(__dirname, './worker/task.js'),
@@ -48,5 +48,107 @@ describe('Dust', () => {
     } catch (error) {
       expect(error.message).toBe('Something went wrong');
     }
+  });
+});
+
+describe('DustContainer execute', () => {
+  const container = new DustContainer();
+
+  it('Test create task and execute with pool', async () => {
+    container.create(
+      'task',
+      workers.task,
+      {
+        worker: { useAbsolute: true },
+        pool: { maxQueuedJobs: 1, size: 1 },
+      },
+    );
+
+    const result = await container.execute(
+      'task',
+      1,
+      2,
+    );
+
+    const result2 = await container.execute(
+      'task2',
+      1,
+      2,
+    );
+
+    expect(result2).toBe(null);
+    expect(result.count).toBe(3);
+    expect(result.isMainThread).toBe(false);
+    await container.close('task');
+  });
+
+  it('Test create async task and execute with pool', async () => {
+    container.create(
+      'async-task',
+      workers['async-task'],
+      {
+        worker: { useAbsolute: true },
+        pool: { maxQueuedJobs: 1, size: 1 },
+      },
+    );
+
+    const result = await container.execute(
+      'async-task',
+      1,
+      2,
+    );
+
+    expect(result.count).toBe(3);
+    expect(result.isMainThread).toBe(false);
+    await container.close('async-task');
+  });
+
+  it('Test create error task and execute with pool', async () => {
+    container.create(
+      'error',
+      workers.error,
+      {
+        worker: { useAbsolute: true },
+        pool: { maxQueuedJobs: 1, size: 1 },
+      },
+    );
+
+    const result = await container.execute('error');
+
+    expect(result).toEqual((null));
+    await container.close('error');
+  });
+});
+
+describe('DustContainer Push', () => {
+  const container = new DustContainer();
+
+  it('Test create task and push with pool but use error path', async () => {
+    try {
+      container.create(
+        'error-task',
+        workers.task,
+      );
+    } catch (error) {
+      expect(!!error).toBe(true);
+    }
+  });
+
+  it('Test create task and push with pool', async () => {
+    container.create(
+      'push-task',
+      workers.task,
+      {
+        worker: { useAbsolute: true },
+      },
+    );
+
+    container.push(
+      'push-task',
+      1,
+      2,
+    );
+
+    await container.close('push-task');
   });
 });
