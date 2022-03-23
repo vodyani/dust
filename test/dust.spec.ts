c162506e02c0de1a296b018408697d107f36ff17
John Denver <chogathkk@gmail.com>
2022-03-23 16:03:45 +0800

    test: add blocking test for thread pools

---
+++ test/dust.spec.ts
@@ -0,0 +1,26 @@
+import { resolve } from 'path';
+
+import { describe, it } from '@jest/globals';
+
+import { Dust } from '../src/common';
+
+const blockingPath = resolve(__dirname, './worker/blocking-test.js');
+
+describe('Dust', () => {
+  it('blocking test', async () => {
+    const handler = new Dust(blockingPath, { poolOptions: { maxQueuedJobs: 2, size: 2 }});
+
+    await Promise.all([
+      handler.push('handler 1'),
+      handler.push('handler 2'),
+      handler.push('handler 3'),
+    ]);
+
+    handler.push('handler 1');
+    handler.push('handler 2');
+    handler.push('handler 3');
+
+    await handler.commit();
+    await handler.close();
+  });
+});
